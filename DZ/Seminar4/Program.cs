b483842c16d1c1af7c2db05a6ac3e38ad16325fd
Nef <flesch_1@mail.ru>
2023-06-15 23:37:01 +0300

    Выполнено задание 4

---
+++ DZ/Seminar4/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(System.Console.ReadLine());
+int b = 0;
+int  index = 0;
+while (b < a)
+{
+    b = b + 1;
+    if (b % 2 == 0) System.Console.Write(b+", ");
+}
