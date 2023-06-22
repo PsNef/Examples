25c5db1ec3968817f83eda38e1fd3948f80391cd
Nef <flesch_1@mail.ru>
2023-06-22 23:09:11 +0300

    Выполнено 3ое домашнее задание

---
+++ DZ/Seminar3/DZ3/Program.cs
@@ -0,0 +1,11 @@
+﻿// 3 -> 1, 8, 27
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите чило");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [a];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (int)Math.Pow(i + 1, 3);
+}
+System.Console.WriteLine(String.Join(", ", array));
