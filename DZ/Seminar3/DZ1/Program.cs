fbad79acd1cd20949b76b8c0694de912d43f027d
Nef <flesch_1@mail.ru>
2023-06-22 21:02:41 +0300

    Выполнено 1ое домашнее задание

---
+++ DZ/Seminar3/DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+System.Console.WriteLine("Введите пятизначное число");
+string a = System.Console.ReadLine()!;
+if (a.Length == 5)
+{
+    if (a [0] == a [4] && a [1] == a [3])
+    {
+        System.Console.WriteLine("Да");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет");
+    }
+}
+else
+{
+    System.Console.WriteLine("Ошбка - введено не 5ти значное число!");
+}
