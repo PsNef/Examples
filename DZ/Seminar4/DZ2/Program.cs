5a658acc646b0b467d342e9d42256feab5aaac5c
Nef <flesch_1@mail.ru>
2023-07-23 16:00:42 +0300

    Выполнено задание №4

---
+++ DZ/Seminar4/DZ2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string Readint (string text)
+{
+    System.Console.WriteLine(text);
+    return Console.ReadLine()!;
+}
+
+int [] NewArray (string array)
+{
+    int [] array1 = new int [array.Length];
+    int i = 0;
+        while (i < array.Length)
+    {
+        array1[i] = Convert.ToInt32(array[i].ToString());
+        i++;
+    }
+    return array1; 
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
+string A = Readint ("Введите массив");
+int [] array = NewArray(A);
+int i = 0;
+int sum = 0;
+while (i < A.Length)
+{
+    sum = sum + array[i];
+    i++;
+}
+
+System.Console.WriteLine(sum);
---
+++ DZ/Seminar4/DZ3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+string Readint (string text)
+{
+    System.Console.WriteLine(text);
+    return Console.ReadLine()!;
+}
+
+int [] NewArray (string array)
+{
+    int [] array1 = new int [array.Length];
+    int i = 0;
+        while (i < array.Length)
+    {
+        array1[i] = Convert.ToInt32(array[i].ToString());
+        i++;
+    }
+    return array1; 
+}
+
+//--------------------------------------------------------------------------------------------------------------------------
+string A = Readint ("Введите массив");
+int [] array = NewArray(A);
+System.Console.WriteLine(String.Join(", ", array));
