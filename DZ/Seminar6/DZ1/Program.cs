5eea4fd4317bbf8fed1c9a4ef4778a34a95e6688
Nef <flesch_1@mail.ru>
2023-07-29 13:58:50 +0300

    Выполнено задание №6

---
+++ DZ/Seminar6/DZ1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] ReadArray(string array)
+{
+    System.Console.WriteLine("Веведите числа, например: 1, 2, -3:");
+    int[] readArray = Array.ConvertAll(System.Console.ReadLine()!.Split(", "), int.Parse);
+    return readArray;
+}
+
+void PrintArray(int[] ArrayForPrin)
+{
+    System.Console.WriteLine("[" + string.Join(", ", ArrayForPrin) + "]");
+}
+
+
+int SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+//---------------------------------------------------------------------------------------------------------
+
+int[] readArray = ReadArray("text");
+int sum  = SumArray(readArray);
+PrintArray(readArray);
+System.Console.WriteLine($"Количество чисел в массиве больше нуля:----->{sum}");
---
+++ DZ/Seminar6/DZ2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int[] ReadArray(string array)
+{
+    System.Console.WriteLine("Веведите числа b1, b2, k1, k2:");
+    int[] readArray = Array.ConvertAll(System.Console.ReadLine()!.Split(", "), int.Parse);
+    return readArray;
+}
+
+void Line(int[] new_Array, out int b1, out int b2, out int k1, out int k2)
+{
+    int b1 = readArray[0];
+    int b2 = readArray[1];
+    int k1 = readArray[2];
+    int k2 = readArray[3];
+}
+//-----------------------------------------------------------------------------
+int[] readArray = ReadArray("text");
+
+Line(readArray, out int b1, out int b2, out int k1, out int k2);
+
+int x = (b2 - b1) / (k1 - k2);
+int y = k1 * x + b1;
+int y2 = k2 * x + b2;
+System.Console.WriteLine(y);
+System.Console.WriteLine(y2);
