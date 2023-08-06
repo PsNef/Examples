711a9c73dc82c8be3ee6c710bad1785f29f64ee8
Nef <flesch_1@mail.ru>
2023-08-06 12:22:33 +0300

    Выполнено задание №9

---
+++ DZ/Seminar9/DZ1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в 
+// промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum (int M, int N)
+{
+    if (M > N)return 0;
+
+    return N + Sum(M, N - 1);
+}
+//-----------------------------------------------------------------------------------------------
+
+int M = ReadInt("Введите число:");
+
+int N = ReadInt("Введите число:");
+
+int sum = Sum(M, N);
+
+System.Console.WriteLine($"Cумма всех чисел равна: {sum}");
---
+++ DZ/Seminar9/DZ2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных 
+// числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum (int M, int N)
+{
+    if (M == 0)return N + 1;
+    if (M !=0 && N == 0)return Sum (M - 1, 1);
+    if (M > 0 && N > 0) return Sum(M - 1, Sum(M, N - 1));
+    return Sum(M, N);
+}
+
+//-----------------------------------------------------------------------------------------------
+
+int M = ReadInt("Введите число:");
+
+int N = ReadInt("Введите число:");
+
+int sum = Sum(M, N);
+
+System.Console.WriteLine($"Cумма всех чисел равна: {sum}");
